3e9191a09a7e447d6bd8329534125c58c9871682
hello-universe <88676596+hello-universe@users.noreply.github.com>
2024-01-20 00:08:19 +0530

    Mainly study createContext and useContext
    Today I made soo many raw react projects because I am not having that much focus but later I deeply studied, code and recorded about createContext and useContext.

---
+++ 6_react_concepts/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+        <ul>
+            <li> <a href="/">Home</a> </li>
+            <li> <a href="/about">About</a> </li>
+            <li> <a href="/contacts">Contacts</a> </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
